d6213b3c23de35cf0d575c4611f51de1f8967633
morningspace <morningspace@yahoo.com>
2020-07-19 12:24:26 +0800

    Update xk.sh

--- xk.sh
+++ xk.sh
@@ -5,7 +5,7 @@
 [[ -n $1 ]] && cmd=$1 || cmd="help"
 
 if [[ -f $(dirname $0)/$cmd.sh ]]; then
-  . $(dirname $0)/$1.sh
+  . $(dirname $0)/$cmd.sh
 else
   logger::error "Command not recognized."
 fi
