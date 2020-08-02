d61d26856582334d4e633277397dae459630ccb3
morningspace <morningspace@yahoo.com>
2020-08-02 18:29:03 +0800

    code reconstruction for context.sh

--- context.sh
+++ context.sh
@@ -1,89 +1,117 @@
 #!/bin/bash
 
+. $(dirname $0)/logs.sh
+
+CONTEXT_CREDENTIALS_FILE=$HOME/.kube/context-credentials
+
+CYAN="\033[0;36m"
+NORMAL="\033[0m"
+RED="\033[0;31m"
+WHITE="\033[0;37m"
+
 mkdir -p $HOME/.kube
-touch $HOME/.kube/context-credentials
+touch $CONTEXT_CREDENTIALS_FILE
 
-function pick_context {
-  local ctx_list=($(kubectl config get-contexts -oname))
-  current_ctx=$(kubectl config current-context)
+function list_context {
+  contexts=($(kubectl config get-contexts -oname))
+  num_of_contexts=${#contexts[@]}
+  current_context=$(kubectl config current-context)
 
-  for ((i = 0; i < ${#ctx_list[@]}; i ++)); do
-    local is_current_ctx=0
-    if [[ $current_ctx == ${ctx_list[$i]} ]]; then
-      is_current_ctx=1
-    fi
+  printf "Contexts:\n\n"
 
-    if cat $HOME/.kube/context-credentials | grep -q ${ctx_list[$i]}; then
-      local entry=($(cat $HOME/.kube/context-credentials | grep ${ctx_list[$i]}))
+  for (( i = 0; i < num_of_contexts; i ++ )); do
+    local context=${contexts[i]}
+    local display_name="$context"
+
+    if cat $CONTEXT_CREDENTIALS_FILE | grep -q $context; then
+      local entry=($(cat $CONTEXT_CREDENTIALS_FILE | grep $context))
       local note=$(echo ${entry[2]} | base64 -d)
-      [[ -n $note ]] && ctx_list[$i]="${ctx_list[$i]} ($note)"
+      [[ -n $note ]] && display_name="$display_name ($note)"
     fi
 
-    if [[ $is_current_ctx == 1 ]]; then
-      ctx_list[$i]="${ctx_list[$i]} *"
+    if [[ $context == $current_context ]]; then
+      printf "%4s) ${CYAN}%s${NORMAL}\n" $i "$display_name *"
+    else
+      printf "%4s) %s\n" $i "$display_name"
     fi
   done
 
-  printf "\033[0;36m? \033[0;37mWhich context do you want to pick up:\033[0m \n"
-  COLUMNS=1
-  PS3="Choose a number: "
-  select ctx in "${ctx_list[@]}"; do
-    case $ctx in
-    *)
-      ctx=${ctx%% *}
-      if [[ -n $ctx && "${ctx_list[@]}" =~ $ctx ]]; then
-        kubectl config use $ctx
-        current_ctx=$(kubectl config current-context)
-        break
-      else
-        printf "Invalid option $REPLY\n"
-      fi;;
-    esac
+  printf "\n"
+}
+
+function pick_context {
+  local num
+
+  while true; do
+    printf "${CYAN}? ${WHITE}Input the number to choose the context you want to switch to:${NORMAL} "
+    read -r num
+
+    if [[ -z $num ]]; then
+      printf "No change has been made, keep using $current_context.\n\n"
+      break
+    fi
+
+    if (( num >= 0 && num < num_of_contexts )); then
+      kubectl config use ${contexts[num]}
+      current_context=$(kubectl config current-context)
+      printf "\n"
+      break
+    else
+      printf "Please input a valid number.\n"
+    fi
   done
 }
 
 function auth_context {
-  # kubectl get users.user.openshift.io >/null 2>&1 || return
-  [[ $current_ctx =~ kind- ]] && return
-  
-  local user_input=0
-  if cat $HOME/.kube/context-credentials | grep -q $current_ctx; then
-    local entry=($(cat $HOME/.kube/context-credentials | grep $current_ctx))
+  local no_auth=0
+  local input_credential=0
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+      --no-auth) no_auth=1; shift ;;
+      --refresh-auth) input_credential=1; shift ;;
+      *) shift ;;
+    esac
+  done
+
+  [[ $no_auth == 1 || $current_context =~ kind- ]] && return
+
+  if [[ $input_credential == 0 ]] && cat $CONTEXT_CREDENTIALS_FILE | grep -q $current_context; then
+    local entry=($(cat $CONTEXT_CREDENTIALS_FILE | grep $current_context))
     local credential=($(echo ${entry[1]} | base64 -d))
     username=${credential[0]}
     password=${credential[1]}
-    if [[ -z $username || -z $password ]]; then
-      user_input=1
-    fi
-  else
-    user_input=1
+    [[ -z $username || -z $password ]] && input_credential=1
   fi
 
-  printf "Find authenticated user: "
-  if ! oc whoami ; then
-    if [[ $user_input == 1 ]]; then
-      echo -n -e "\033[0;36m? \033[0;37mUsername: \033[0m"
-      read -r username
-      echo -n -e "\033[0;36m? \033[0;37mPassword: \033[0m"
-      read -s password
-      echo
-    fi
+  if [[ $input_credential == 1 ]]; then
+    printf "${CYAN}? ${WHITE}Username: ${NORMAL}"
+    read -r username
+    printf "${CYAN}? ${WHITE}Password: ${NORMAL}"
+    read -s password
+    printf "\n"
+  fi
 
-    oc login -u $username -p $password
+  printf "Check if user has logged in ... "
+
+  if oc whoami; then
+    printf "\n"
+  else
+    printf "\n"
+    oc login -u $username -p $password || return 1
   fi
 }
 
 function note_context {
-  echo -n -e "\033[0;36m? \033[0;37mAdd a note or press Enter to skip: \033[0m"
+  printf "${CYAN}? ${WHITE}Add a note or press Enter to skip: ${NORMAL}"
   read -r note
 }
 
 function save_context {
   local credential=$(echo "$username $password" | base64 -w 0)
-  local context_info="$current_ctx $credential"
+  local context_info="$current_context $credential"
   if [[ -z $note ]]; then
-    if cat $HOME/.kube/context-credentials | grep -q $current_ctx; then
-      local entry=($(cat $HOME/.kube/context-credentials | grep $current_ctx))
+    if cat $CONTEXT_CREDENTIALS_FILE | grep -q $current_context; then
+      local entry=($(cat $CONTEXT_CREDENTIALS_FILE | grep $current_context))
       note=${entry[2]}
     fi
   else
@@ -91,15 +119,50 @@ function save_context {
   fi
   context_info="$context_info $note"
 
-  if cat $HOME/.kube/context-credentials | grep -q $current_ctx; then
-    cat ~/.kube/context-credentials | sed "s#^$current_ctx.*#$context_info#" > ~/.kube/context-credentials.tmp
+  if cat $CONTEXT_CREDENTIALS_FILE | grep -q $current_context; then
+    cat ~/.kube/context-credentials | sed "s#^$current_context.*#$context_info#" > ~/.kube/context-credentials.tmp
     mv ~/.kube/context-credentials{.tmp,}
   else
-    echo "$context_info" >> $HOME/.kube/context-credentials
+    echo "$context_info" >> $CONTEXT_CREDENTIALS_FILE
   fi
 }
 
-pick_context
-auth_context
-note_context
-save_context
+function change_context {
+  list_context
+  pick_context
+  auth_context $@ && note_context && save_context
+}
+
+function delete_context {
+  list_context
+
+  local num
+  while true; do
+    printf "${CYAN}? ${WHITE}Input the number to choose the context you want to delete:${NORMAL} "
+    read -r num
+
+    if [[ -z $num ]]; then
+      kubectl config delete-context $current_context
+      break
+    fi
+
+    if (( num >= 0 && num < num_of_contexts )); then
+      kubectl config delete-context ${contexts[num]}
+      break
+    else
+      printf "Please input a valid number.\n"
+    fi
+  done
+}
+
+handle=list
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -l) handle=list; shift ;;
+    -c) handle=change; shift ;;
+    -d) handle=delete; shift ;;
+    *)  POSITIONAL+=("$1"); shift ;;
+  esac
+done
+
+${handle}_context ${POSITIONAL[@]}
