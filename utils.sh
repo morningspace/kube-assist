ffbad0caf456e567f0bca4b6d8a167163bf73f48
morningspace <morningspace@yahoo.com>
2020-08-03 09:10:41 +0800

    Rename logs.sh to utils.sh

---
+++ utils.sh
@@ -0,0 +1,23 @@
+CYAN="\033[0;36m"
+NORMAL="\033[0m"
+RED="\033[0;31m"
+YELLOW="\033[0;33m"
+WHITE="\033[0;37m"
+
+function logger::info {
+  printf "${CYAN}INFO ${NORMAL} $@\n"
+}
+
+function logger::warn {
+  printf "${YELLOW}WARN ${NORMAL} $@\n"
+}
+
+function logger::error {
+  printf "${RED}ERROR${NORMAL} $1\n"
+}
+
+mkdir -p $HOME/.ka
+
+function on_exit {
+  rm -rf $HOME/.cp4mcm
+}
