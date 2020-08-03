253ddfce405c175a1ac58fb31ff53ca3f90d972d
morningspace <morningspace@yahoo.com>
2020-08-03 17:08:51 +0800

    Code refactor for ka.sh

--- ka.sh
+++ ka.sh
@@ -3,15 +3,17 @@
 command_name=${1:-help}
 
 case $command_name in
-  context|pods|logs|help) ;;
-  ctx) command_name="context" ;;
-  pod|po) command_name="pods" ;;
-  log) command_name="logs" ;;
-  *)
-    echo 'Argument "'$command_name'" not known.'
-    command_name="help" ;;
+  context|pods|logs|cert|help)    ;;
+  ctx)    command_name="context"  ;;
+  pod|po) command_name="pods"     ;;
+  log)    command_name="logs"     ;;
+  *)      echo 'Argument "'$command_name'" not known.'
+          command_name="help"     ;;
 esac
 
-command_file=$(dirname $0)/commands/$command_name.sh
-
-[[ -f $command_file ]] && $command_file ${@:2}
+command_script=$(dirname $0)/commands/$command_name.sh
+if [[ -x $command_script ]]; then
+  $command_script ${@:2}
+else
+  echo 'File "'$command_script'" not found or not excutable.'
+fi
